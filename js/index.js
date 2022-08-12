219e225394fc6b0fc388ac5d38e639638a4224bc
madiiwill97 <madisonkawilliams@gmail.com>
2022-08-12 11:09:15 -0500

    Homework 19 commit
    jQuery hover interaction added in nav bar

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(".nav-link").hover(function () {
+  $(this).toggleClass("hover");
+  console.log("hovering!");
+});
+// $(".nav-link").hover("mouseleaves", function () {
+//   $(this).removeClass("hover");
+// });
+// $("a").on("click", function () {
+//     $(this).addClass("hover)");
+//   });
