f3c1256d4400092c7edb644918d68dd1a337c59a
madiiwill97 <madisonkawilliams@gmail.com>
2022-08-12 11:25:44 -0500

    deleted unnecessary files

--- js/index.js
+++ js/index.js
@@ -2,9 +2,3 @@ $(".nav-link").hover(function () {
   $(this).toggleClass("hover");
   console.log("hovering!");
 });
-// $(".nav-link").hover("mouseleaves", function () {
-//   $(this).removeClass("hover");
-// });
-// $("a").on("click", function () {
-//     $(this).addClass("hover)");
-//   });
