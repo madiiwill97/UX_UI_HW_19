506b9b32af1b372887bf3a6a995a9b344d7dc4cd
madiiwill97 <madisonkawilliams@gmail.com>
2022-08-25 14:31:53 -0500

    linked to hw20

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ $(".nav-link").hover(function () {
   $(this).toggleClass("hover");
   console.log("hovering!");
 });
+$(".nav-link2").hover(function () {
+  $(this).toggleClass("hover2");
+  console.log("hovering!");
+});
